9a96edb76e47a4a58542a7d8d02cb7ff9516b277
Helio Henrique <heliohenriquemj@hotmail.com>
2021-11-18 18:13:44 -0400

    lista de tarefas

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
+  final List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newTodo = {};
+      newTodo["Title"] = _toDoController.text;
+      _toDoController.text = "";
+      newTodo["Ok"] = false;
+      _toDoList.add(newTodo);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: _addToDo,
+                    child: const Text(
+                      "ADD",
+                    ))
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["Title"]),
+                    value: _toDoList[index]["Ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                          _toDoList[index]["Ok"] ? Icons.check : Icons.error),
+                    ),
+                    onChanged: (bool? value) {},
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String?> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+}
