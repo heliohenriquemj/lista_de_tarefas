35c784f334fd022849512151ace890bc43c51eac
Helio Henrique <heliohenriquemj@hotmail.com>
2021-11-19 18:57:54 -0400

    lista de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -76,24 +76,39 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
                 padding: const EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["Title"]),
-                    value: _toDoList[index]["Ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(
-                          _toDoList[index]["Ok"] ? Icons.check : Icons.error),
-                    ),
-                    onChanged: (c) {
-                      setState(() {
-                        _toDoList[index]["Ok"] = c;
-                        _saveData();
-                      });
-                    },
-                  );
-                }),
+                itemBuilder: buildItem),
           )
         ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: const Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["Title"]),
+        value: _toDoList[index]["Ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["Ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["Ok"] = c;
+            _saveData();
+          });
+        },
       ),
     );
   }
